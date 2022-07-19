e4c3d53e4587a0b2d0db2485ffce0e654ebc4ac3
Aleksandra Kameneva <kameneva_fkp@mail.ru>
2022-07-19 17:39:29 +0500

    ДЗ 5 исправления

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,53 +1,8 @@
 import Character from '../app';
-import Bowman from '../bowman';
-import Swordsman from '../swordsman';
-import Magician from '../magician';
-import Daemon from '../daemon';
-import Undead from '../undead';
-import Zombie from '../zombie';
 
-test('should error', () => {
-  expect(() => new Character()).toThrow('...');
+test('Ошибка в name', () => {
+  expect(() => new Character()).toThrow('Ошибка в name');
 });
-
-test('should Bowman', () => {
-  const expected = 25;
-  const res = new Bowman('Иван');
-  const response = res.defence;
-  expect(response).toBe(expected);
-});
-
-test('should Swordsman', () => {
-  const expected = 10;
-  const res = new Swordsman('Иван');
-  const response = res.defence;
-  expect(response).toBe(expected);
-});
-
-test('should Magician', () => {
-  const expected = 40;
-  const res = new Magician('Иван');
-  const response = res.defence;
-  expect(response).toBe(expected);
-});
-
-test('should Daemon', () => {
-  const expected = 40;
-  const res = new Daemon('Иван');
-  const response = res.defence;
-  expect(response).toBe(expected);
-});
-
-test('should Undead', () => {
-  const expected = 25;
-  const res = new Undead('Иван');
-  const response = res.defence;
-  expect(response).toBe(expected);
-});
-
-test('should Zombie', () => {
-  const expected = 10;
-  const res = new Zombie('Иван');
-  const response = res.defence;
-  expect(response).toBe(expected);
+test('Ошибка в type', () => {
+  expect(() => new Character('Name', 'Magician1')).toThrow('Ошибка в type');
 });
